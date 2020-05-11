abf09faa2cabab3a772ef2e58cfa52b3448caad2
Ved Verma <ved.verma@greenlightcommerce.com>
2020-05-11 15:15:53 +0100

    Finished CoinDenomination

--- src/dynamicprogramming/CoinDenomination.java
+++ src/dynamicprogramming/CoinDenomination.java
@@ -2,6 +2,8 @@ package dynamicprogramming;
 
 public class CoinDenomination {
 
+   //Time complexity - O(n*d) - where n is total amount and d is denominations
+   //Space complexity - O(n)
    public int getTotalWays(int[] denominations, int totalAmount){
 
       int[] totalWays = new int[totalAmount + 1];
@@ -12,19 +14,19 @@ public class CoinDenomination {
       for(int coin : denominations)
          getWays(coin, totalAmount, totalWays);
 
-      return 0;
+      return totalWays[totalAmount];
    }
 
    private void getWays(int coin, int totalAmount, int[] totalWays) {
       for(int i = 1; i <= totalAmount; i ++){
-         if(totalAmount >= coin)
-            totalWays[totalAmount] = 1 + totalWays[totalAmount]
+         if(i >= coin)
+            totalWays[i] += totalWays[i-coin];
       }
    }
 
    public static void main(String[] args) {
       CoinDenomination obj = new CoinDenomination();
-      int[] denominations = {1,2,5,10,25};
+      int[] denominations = {1,5,10,25};
       int totalAmount = 10;
       System.out.print(obj.getTotalWays(denominations, totalAmount));
    }
